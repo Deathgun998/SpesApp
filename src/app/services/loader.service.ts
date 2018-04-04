f55faf259fa390d1b2a415bfdd012dce6982a1e9
Deathgun998 <davi.fuma@hotmail.com>
2018-04-04 11:56:20 +0200

    importato il loaderservice

---
+++ src/app/services/loader.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Subject }    from 'rxjs/Subject';
+
+@Injectable()
+export class LoaderService {
+    counter: number = 0;
+    public loaderVisible: Subject<boolean> = new Subject<boolean>();;
+
+    showLoader(): void {
+        this.incrLoader();
+        if (this.getCounterLoader() == 1) {
+            this.loaderVisible.next(true);
+        }
+    };
+
+    hideLoader(): void {
+        this.decrLoader();
+        if (this.getCounterLoader() == 0) {
+            this.loaderVisible.next(false);
+        }
+    };
+
+    hideAndResetLoader(): void {
+        this.setCounterLoader(0);
+        this.loaderVisible.next(false);
+    };
+
+    incrLoader(): void {
+        this.counter++;
+    };
+
+    decrLoader(): void {
+        if (this.counter > 0)
+            this.counter--;
+    };
+
+    getCounterLoader(): number {
+        return this.counter;
+    };
+
+    setCounterLoader(value: number): void {
+        this.counter = value;
+    };
+  }
